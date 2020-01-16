045fe1aa5a5ad7f818ad8a99093d67b373a2d479
AmitAzadi <56012894+AmitAzadi@users.noreply.github.com>
2020-01-16 22:04:59 +0530

    Update smallest pair solution
    its a shortcode and easy to understand by the beginners.

--- The Smallest Pair.cpp
+++ The Smallest Pair.cpp
@@ -1,29 +1,16 @@
-//CodeChef
-//Beginner Practice Problem
-//The Smallest Pair [SMPAIR]
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-unsigned long long t=0;
-cin>>t;
-
-while(t--)
-    {
-    unsigned long long n=0;
-    cin>>n;
-
-    vector <unsigned long long> v;
-    for(unsigned long long i=0;i<n;i++)
-        {
-        unsigned long long a=0;
-        cin>>a;
-        v.push_back(a);
-        }
-    sort(v.begin(),v.end());
-    cout<<v[0]+v[1]<<"\n";
-    }
-return 0;
+	int n,x;
+	cin>>x;
+	while(x--)
+	{
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		sort(a,a+n);
+		cout<<a[0]+a[1]<<endl;
+	}
 }
